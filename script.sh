5652960b3d60972da7541df07e45b7d7aa33e964
utopikkad <45292849+utopikkad@users.noreply.github.com>
2021-01-18 14:47:00 +0100

    ports fixing

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 # spin flask image
 cd flask_app
-docker build -t my-nginx:latest .
+docker build -t app:latest .
 
 # spin nginx image
 cd ../nginx
-docker build -t app:latest .
+docker build -t my-nginx:latest .
 
 # spin the infra
 cd ..
